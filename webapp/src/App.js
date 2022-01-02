56647084a832e255e2b077d14f37deb200105af0
Crehan <crehanas14@gmail.com>
2022-01-02 12:14:44 +0530

    add form for URL and image input

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { Navbar , Container , Nav , Form , Button , Collapse, Card , Row , Col } from 'react-bootstrap';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [ openUrl , setOpenUrl ] = useState(false);
+    const [ openImage , setOpenImage ] = useState(false);
+    const [ tweetUrl , setTweetUrl ] = useState('');
+    return (
+    <>
+        <Navbar bg="dark" variant="dark">
+            <Container>
+                <Navbar.Brand href="/">offensive-content-detection</Navbar.Brand>
+            </Container>
+        </Navbar>
+        <Container className='mt-5 w-50'>
+            <Card className='shadow'>
+                <Card.Header className='d-flex justify-content-center'>
+                        <Button
+                            className='w-25 mx-2 my-2'
+                            onClick={() => {
+                                if(openUrl){
+                                    setOpenUrl(false)
+                                }else{
+                                    setOpenUrl(true)
+                                    setOpenImage(false)
+                                }
+                            }}
+                            aria-controls="url-collapse"
+                            aria-expanded={openUrl}
+                        >
+                            URL
+                        </Button>
+                        <Button
+                            className='w-25 mx-2 my-2'
+                            onClick={() => {
+                                if(openImage){
+                                    setOpenImage(false)
+                                }else{
+                                    setOpenUrl(false)
+                                    setOpenImage(true)
+                                }
+                            }}
+                            aria-controls="image-collapse"
+                            aria-expanded={openImage}
+                        >
+                        Upload Image
+                        </Button>
+                </Card.Header>
+                <div>
+                    <Collapse in={openUrl}>
+                        <div id="url-collapse">
+                        <Form.Group className="m-3">
+                            <Form.Label>Enter URL of the Tweet to be Checked</Form.Label>
+                            <Form.Control
+                                type="text"
+                                placeholder="Enter URL"
+                                value = {tweetUrl}
+                                onChange={e => setTweetUrl(e.target.value)}
+                            />
+                        </Form.Group>
+                        </div>
+                    </Collapse>
+                    <Collapse in={openImage}>
+                        <div id="image-collapse">
+                            <Form.Group className="m-3">
+                                <Form.Label>Upload the Image file to be checked</Form.Label>
+                                <Form.Control type="file" />
+                            </Form.Group>
+                        </div>
+                    </Collapse>
+                </div>
+
+            </Card>
+        </Container>
+    </>
+    );
 }
 
 export default App;
